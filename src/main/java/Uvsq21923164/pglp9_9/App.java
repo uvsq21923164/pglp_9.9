3d3c343d13d0002f30b07902ed64be03bab81b97
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-05-07 02:19:31 +0200

    creation de projet maven

---
+++ src/main/java/Uvsq21923164/pglp9_9/App.java
@@ -0,0 +1,13 @@
+package Uvsq21923164.pglp9_9;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
