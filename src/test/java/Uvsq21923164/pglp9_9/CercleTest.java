aa19ebecc1dd644bd43be514a73fc1152206c026
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-05-15 05:06:22 +0200

    ajouter test cercle

---
+++ src/test/java/Uvsq21923164/pglp9_9/CercleTest.java
@@ -0,0 +1,20 @@
+package Uvsq21923164.pglp9_9;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CercleTest {
+
+	@Test
+	public void test() {
+		Cercle c1 = new Cercle ("youyou",new Point(2,3), 4);
+		c1.deplacer("youyou", 5, 2);
+		assertEquals(c1.getCentre().getX(),7);
+		assertEquals(c1.getCentre().getY(),5);
+		c1.deplacer("youyou", 2, 2);
+		assertEquals(c1.getCentre().getX(),9);
+		assertEquals(c1.getCentre().getY(),7);
+	
+}
+}
