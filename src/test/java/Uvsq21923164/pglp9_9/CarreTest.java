625e727be27a4ea23568c87a9a90a00ac73e38a6
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-05-15 05:04:59 +0200

    ajouter test caree

---
+++ src/test/java/Uvsq21923164/pglp9_9/CarreTest.java
@@ -0,0 +1,21 @@
+package Uvsq21923164.pglp9_9;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CarreTest {
+
+
+	@Test
+	public void test() {
+		Carre c1 = new Carre ("youyoyu",new Point(2,2), 5);
+		c1.deplacer("youyou", 2, 1);
+		assertEquals(c1.getPoint().getX(),4);
+		assertEquals(c1.getPoint().getY(),3);
+		c1.deplacer("youyou", 3, 6);
+		assertEquals(c1.getPoint().getX(),7);
+		assertEquals(c1.getPoint().getY(),9);
+	}
+
+}
