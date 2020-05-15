a688ff599ee3ce44849fa1f4e498b8983acbe146
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-05-15 04:49:19 +0200

    creation classe Forme

---
+++ src/main/java/Uvsq21923164/pglp9_9/Forme.java
@@ -0,0 +1,37 @@
+package Uvsq21923164.pglp9_9;
+
+
+
+/*  
+ *Implementation de la class Forme
+ * */
+public abstract class Forme {
+	String name;
+	/* constructeur */
+	public Forme(String name) {
+		this.name = name;
+	}
+	
+/*	methode get */
+	
+	 public String getname() {
+	        return name;
+	    }
+	 /* methode set */
+	 public void setNom(String name) {
+			this.name= name;
+		}
+	 public abstract void deplacer(String name, int x, int y);
+	    /**
+	     * affichage de la forme.
+	     */
+	    public abstract void affiche();
+	
+	
+	
+	
+	
+	
+	
+	
+}
