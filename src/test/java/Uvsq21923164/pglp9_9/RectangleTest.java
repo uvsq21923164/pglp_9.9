d667386b427165a326b687cdda019c54ddffe8ce
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-05-15 05:07:20 +0200

    ajouter test rectangle

---
+++ src/test/java/Uvsq21923164/pglp9_9/RectangleTest.java
@@ -0,0 +1,19 @@
+package Uvsq21923164.pglp9_9;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RectangleTest {
+
+	@Test
+	public void test() {
+		Rectangle c1 = new Rectangle ("youcef",new Point(3,2),2,5);
+		c1.deplacer("youcef",2, 2);
+		assertEquals(c1.getPoint().getX(),5);
+		assertEquals(c1.getPoint().getY(),4);
+		c1.deplacer("youcef", 1, 8);
+		assertEquals(c1.getPoint().getX(),6);
+		assertEquals(c1.getPoint().getY(),12);
+	}
+}
