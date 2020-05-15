ccfe0375077557a848288cb953d901712da40f8c
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-05-16 00:02:57 +0200

    ajouter test pour la classe formegroupe

---
+++ src/test/java/Uvsq21923164/pglp9_9/FormeGroupeTest.java
@@ -0,0 +1,14 @@
+package Uvsq21923164.pglp9_9;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FormeGroupeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
