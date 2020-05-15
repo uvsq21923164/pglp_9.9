bda072a94ca2c8ec07ee3d785af5caae7df7b320
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-05-15 04:58:24 +0200

    creation classe Recangle et les methodes get ,set ,affiche et deplacer

---
+++ src/main/java/Uvsq21923164/pglp9_9/Rectangle.java
@@ -0,0 +1,84 @@
+package Uvsq21923164.pglp9_9;
+
+public class Rectangle extends Forme{
+	int largeur,longeur;
+	Point point;
+	public Point getPoint() {
+		return point;
+	}
+	/* methode get et set */
+	public void setPoint(Point point) {
+		this.point = point;
+	}
+	public int getLargeur() {
+		return largeur;
+	}
+	public void setLargeur(int largeur) {
+		this.largeur = largeur;
+	}
+	public int getLongeur() {
+		return longeur;
+	}
+	public void setLongueur(int longeur) {
+		this.longeur = longeur;
+	}
+
+	/* construcreur */
+	 public Rectangle(String name,Point p,int lag,int log) {
+	    	super(name);
+	    	point=p.copie();
+	    	this.largeur=lag;
+	    	this.longeur=log;
+	    	
+	    }
+	@Override
+	public void deplacer(String name, int x, int y) {
+		// TODO Auto-generated method stub
+		point.deplacer(x, y);
+	}
+	@Override
+	public void affiche() {
+		// TODO Auto-generated method stub
+		System.out.println("Rectangle : " + name + ", Point :" + point.toString()+", Longueur : "+longeur+", largeur :"+largeur);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
